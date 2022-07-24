f68bb61092fc0c0ad69c5df1f1d19e2c421a42f4
Albin Perroj <albinperroj@gmail.com>
2022-07-24 23:25:04 +0200

    clean project

--- obj/Debug/QRCodeGenerator_Content.g.i.cs
+++ obj/Debug/QRCodeGenerator_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("resources/icons/attach.ico")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute(".gitignore")]
 
 
